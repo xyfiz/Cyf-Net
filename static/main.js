53fa9d494d35278665ce59d9604627fc865fa044
itscyfix <itscyfix@gmail.com>
2022-07-03 01:41:15 +0530

    adding files via cli

---
+++ static/main.js
@@ -0,0 +1,14 @@
+console.log("hello world!");
+function lol(){
+    document.getElementById('yes').style.backgroundColor = "yellow"
+}
+function lmao(){
+    document.getElementById('yes').style.backgroundColor = "red"
+}
+function lole(){
+    document.getElementById('yes').style.backgroundColor = "black"
+}
+function lolrev(){
+    document.getElementById('yes').style.backgroundColor = "#8db9be"
+}
+
